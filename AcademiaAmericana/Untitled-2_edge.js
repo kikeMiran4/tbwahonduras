f3fb9c0e27a64c0f1c260911fadff77747065f49
kikeMiran4 <jorgeenrrique16@hotmail.com>
2019-08-06 07:42:56 -0600

    ok

--- AcademiaAmericana/Untitled-2_edge.js
+++
@@ -1,7 +0,0 @@
-
-(function(compId){var _=null,y=true,n=false,x1='6.0.0',x3='6.0.0.400',x2='5.0.0',x5='rgba(0,0,0,0)',w='width',g='image',x4='horizontal',x7='rgba(255,255,255,1)';var g6='DONA-OLGA1.png';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:w,cg:x4,rI:n,cn:{dom:[{id:'DONA-OLGA1',t:g,r:['-13px','-7px','4151px','1927px','auto','auto'],f:[x5,im+g6,'0px','0px']}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'1080px','1920px'],overflow:'hidden',f:[x7]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-489546231");
-(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
-//Edge symbol: 'stage'
-(function(symbolName){})("stage");
-//Edge symbol end:'stage'
-})})(AdobeEdge.$,AdobeEdge,"EDGE-489546231");
