2d99baa27bd784be3852e0c708c0b8ebd33d775f
Jorge Miranda <jorgeenrrique16@hotmail.com>
2019-11-22 14:46:39 -0600

    ok

--- test/Untitled-1_edge.js
+++
@@ -1,7 +0,0 @@
-
-(function(compId){var _=null,y=true,n=false,x1='6.0.0',x3='6.0.0.400',x2='5.0.0',x5='rgba(0,0,0,0)',x7='rgba(255,255,255,1)',g='image',x4='both';var g6='Untitled-2.png';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:x4,cg:x4,rI:n,cn:{dom:[{id:'Untitled-2',t:g,r:['0px','0px','1920px','834px','auto','auto'],f:[x5,im+g6,'0px','0px']}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'1920px','834px'],overflow:'hidden',f:[x7]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-1238895");
-(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
-//Edge symbol: 'stage'
-(function(symbolName){})("stage");
-//Edge symbol end:'stage'
-})})(AdobeEdge.$,AdobeEdge,"EDGE-1238895");
